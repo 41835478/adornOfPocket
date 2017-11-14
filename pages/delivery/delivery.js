aa79b57ce7b5892d9d372a84609fba4e017c0059
Cao yang <caoyang@CaodeMacBook-Pro.local>
2017-11-14 11:00:59 +0800

    修改地址列表bug

--- pages/delivery/delivery.js
+++ pages/delivery/delivery.js
@@ -5,11 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    url: 'mall/wx/delivery/list',
+    url: 'mall/wx/delivery/findByWxCode',
 
     deliveryList: [],
     selectId: 0,
-    pageNo:1
+    pageNo: 1,
+    lastPage: false,
+    completData:false
   },
   /**
    * input输入
@@ -33,52 +35,65 @@ Page({
    */
   skipPage(val) {
     let id = val.currentTarget.id
-    if(id == 'add'){
+    if (id == 'add') {
       let addUrl = '/pages/delivery/deliveryDetail/deliveryDetail?flag=1'
       wx.navigateTo({
         url: addUrl,
       })
     }
-    if(id == 'modify'){
-      console.log("val=",val)
+    if (id == 'modify') {
+      console.log("val=", val)
       var modityData = val.currentTarget.dataset.address
-      let modifyUrl = '/pages/delivery/deliveryDetail/deliveryDetail?flag=0&id=' + modityData.id+'&data='+JSON.stringify(modityData)
+      let modifyUrl = '/pages/delivery/deliveryDetail/deliveryDetail?flag=0&id=' + modityData.id + '&data=' + JSON.stringify(modityData)
       wx.navigateTo({
         url: modifyUrl,
       })
     }
   },
   /**
-   * 网络获取地址信息 style: 0 获取第1页5条数据 1 上拉获取数据
+   * 网络获取地址信息
    */
-  getDataFromNet(style) {
-    let that = this
+  getDataFromNet() {
     wx.showLoading({
       title: '加载中',
     })
-    let pageNo 
-    if(style == 0){
-      pageNo = 1;
-    }else if(style == 1){
-      pageNo = that.data.pageNo +1
+    let that = this
+    console.log("是否最后一页:",that.data.lastPage)
+    if (that.data.lastPage) {
+      wx.hideLoading()
+      return
+    }
+    
+    let pageNo
+    if (that.data.completData) {
+      pageNo = that.data.pageNo + 1;
+    } else {
+      pageNo = 1
     }
     wx.login({
       success: res => {
         console.log(res);
         if (res.code) {
-          let url = getApp().globalData.baseUrl + that.data.url + "?wxCode=" + res.code + "&pageNo="+ pageNo +"&pageSize=5"
-          console.log("url=" +url)
+          let url = getApp().globalData.baseUrl + that.data.url + "?wxCode=" + res.code + "&pageNo=" + pageNo + "&pageSize=5"
+          console.log("url=" + url)
           if (res.code) {
             wx.request({
               url: url,
               success: res => {
                 wx.hideLoading()
-                let str =JSON.stringify(res.data)
-                console.log("收货地址:" + str)
-                that.setData({
-                  deliveryList: res.data.list,
-                  pageNo: pageNo
-                })
+                let arr = that.data.deliveryList.concat(res.data.list)
+                if (res.data.list.length > 0) {
+                  that.setData({
+                    deliveryList: res.data.list,
+                    pageNo: pageNo,
+                    completData:true
+                  })
+                } else {
+                  that.setData({
+                    lastPage: true,
+                  })
+                }
+
               },
               fail: err => {
                 wx.hideLoading()
@@ -110,14 +125,17 @@ Page({
    */
   onShow: function () {
 
-    this.getDataFromNet(0)
+    this.getDataFromNet()
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+      this.setData({
+        lastPage:false,
+        completData:false
+      })
   },
 
   /**
@@ -131,14 +149,18 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    // this.getDataFromNet()
 
+    // this.getDataFromNet(0)
+    // wx.stopPullDownRefresh()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    this.getDataFromNet(1)
+    this.getDataFromNet()
+    // wx.stopPullDownRefresh()
   },
 
   /**
